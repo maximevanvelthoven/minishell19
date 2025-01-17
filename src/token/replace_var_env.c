72e4d171708471e6595970241be71b6bc26bc466
Sami Soumillion <ssoumill@shi-r3-p5.s19.be>
2025-01-17 19:25:16 +0100

    save change

--- src/token/replace_var_env.c
+++ src/token/replace_var_env.c
@@ -6,7 +6,7 @@
 /*   By: ssoumill <ssoumill@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 17:51:41 by ssoumill          #+#    #+#             */
-/*   Updated: 2025/01/15 18:30:10 by ssoumill         ###   ########.fr       */
+/*   Updated: 2025/01/17 19:21:55 by ssoumill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,12 @@ void	replace_var_env(t_env **l_word, t_data *data)
 {
 	t_env	*context;
 	t_env	*tmp;
+	t_env  *tmp2;
 
 	context = *l_word;
 	while (context)
 	{
+		tmp = context->next;
 		if (context->value[0] == '$' && context)
 		{
 			if (context->value[1] == '?')
@@ -74,14 +76,14 @@ void	replace_var_env(t_env **l_word, t_data *data)
 				replace_node(&context, data);
 			else
 			{
-				tmp->next = context->next;
-				free(tmp->value);
-				free(context->value);
+				tmp2 = context;
+				free(tmp2->value);
+				free(tmp2);
+				context = tmp;
+				//free(tmp->value);
 				//free(context); // free context en egalant a une tmp
 			}
 		}
-		tmp = context;
-		free(tmp);
 		context = context->next;
 	}
 }
